dc1f6265c9e447a9e073a6d7ea95f63570d6cdbc
Bjorn Geerding <bjorn.geerding@polteq.com>
2018-07-26 14:17:56 +0200

    Exercise 6.02

--- src/test/java/chapterSix/FillCartTest.java
+++ src/test/java/chapterSix/FillCartTest.java
@@ -5,13 +5,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FillCartTest {
 
     @Test
-    public void FillCart() {
+    public void FillCart() throws InterruptedException {
         ChromeDriverManager.getInstance().setup();
         WebDriver driver = new ChromeDriver();
 
@@ -20,6 +22,18 @@ public class FillCartTest {
 
         WebElement shoppingCartEmpty = driver.findElement(By.className("ajax_cart_no_product"));
         Assert.assertTrue(shoppingCartEmpty.isDisplayed());
+
+        driver.findElement(By.className("first_item")).click();
+        driver.findElement(By.xpath("//a[text() = ' iPod shuffle ']")).click();
+        driver.findElement(By.id("add_to_cart")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("layer_cart"))));
+
+        driver.findElement(By.xpath("//span[text() = 'Continue shopping ']")).click();
+
+        WebElement shoppingCartFilled = driver.findElement(By.className("ajax_cart_quantity"));
+        Assert.assertEquals(shoppingCartFilled.getText(), "1");
 
         driver.quit();
     }
